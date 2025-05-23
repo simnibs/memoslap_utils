36fb8a16fdc247dd2fd44c7c8eab2d1ddd5f6609
axelthi <155726363+axelthi@users.noreply.github.com>
2025-05-23 11:42:40 +0200

    Update example_get_distances.py

--- examples/example_get_distances.py
+++ examples/example_get_distances.py
@@ -8,7 +8,7 @@ import sys
 
 # --------------------------------------------------
 # NOTE: Update this path to fit to your installation
-path_to_utils = 'C:/Users/axthi/simnibsN/memoslap/'
+path_to_utils = 'C:/Users/axthi/simnibsN/memoslap_utils/'
 sys.path.append(path_to_utils)
 from simnibs_memoslap_utils.utils import get_scalp_skull_csf_thickness_for_subject
 
